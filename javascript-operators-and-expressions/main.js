8b0d74b845039e7d5cdd1352641a0c85c54cbbde
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-02-14 21:37:05 -0800

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 10;
+var area = height * width;
+console.log('the value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 20;
+var payment = 25;
+var change = payment - bill;
+console.log('the value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 90;
+var midterm = 100;
+var final = 95;
+var grade = (quizzes + midterm + final) / 3;
+console.log('the value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Darren';
+var lastName = 'Yang';
+var fullName = firstName + ' ' + lastName;
+console.log('the value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 12;
+var isAcidic = pH < 7;
+console.log('the value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 30;
+var isSparta = headCount === 300;
+console.log('the value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('the value of motto:', motto);
+console.log('typeof motto:', typeof motto);
