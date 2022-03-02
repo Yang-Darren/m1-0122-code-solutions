2072cc9582ad17114f4e0b73c06f2efb7b8b3c1f
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-03-01 20:15:05 -0800

    Completed string-manipulation-1 exercise.

---
+++ string-manipulation-1/src/is-upper-cased.js
@@ -0,0 +1,8 @@
+/* exported isUpperCased */
+
+// define a function isUpperCased with one parameter word
+// return toUpperCase() method of the word object is strictly equal to word
+
+function isUpperCased(word) {
+  return word.toUpperCase() === word;
+}
