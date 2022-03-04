692c1c6059e56947d84121e60c5295a81914e2ac
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-03-03 19:30:53 -0800

    Completed object-manipulation-1 exercise.

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,7 @@
+/* exported getValue */
+// define a function getValue with two parameters object, and key
+// the key method of the object, object is return in bracket notation
+
+function getValue(object, key) {
+  return object[key];
+}
