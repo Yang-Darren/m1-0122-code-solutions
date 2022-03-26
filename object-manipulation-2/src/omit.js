a455b485e3cdebd1d9ab76239676576e9448a45f
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-03-25 19:55:32 -0700

    Completed object-manipulation-2 exercise.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,10 +1,9 @@
 /* exported omit */
 function omit(source, keys) {
-  var newObject = {};
-  for (var i = 0; i < keys.length; i++) {
-    if (newObject[keys[i]] !== source[keys[i]]) {
-      newObject.push(source[keys[i]]);
-    }
+  var newObj = {};
+  Object.assign(newObj, source);
+  for (let i = 0; i < keys.length; i++) {
+    delete newObj[keys[i]];
   }
-  return newObject;
+  return newObj;
 }
